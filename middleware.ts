a34be8f4707c567427c8cdd51b35851ac2b854c0
stipica19 <stipica.klepic@mangoart.at>
2026-01-17 14:51:08 +0100

    middleware

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
-export { default } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-export const config = {
-    matcher: ["/admin/:path*"],
-};
+export function middleware(req: NextRequest) {
+    return NextResponse.next();
+}
