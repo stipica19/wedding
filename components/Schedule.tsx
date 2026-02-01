872bff0eafcf8b7f796fea511771af02bb2a8935
stipica19 <pipiklepic1@gmail.com>
2026-02-01 16:29:32 +0100

    ringg

--- components/Schedule.tsx
+++ components/Schedule.tsx
@@ -1,5 +1,3 @@
-import prsten from "@/public/ring.png";
-
 import Reveal from "./Reveal";
 import BestekIcon from "./BestekIcon";
 
@@ -16,7 +14,7 @@ function ScheduleItem({ href, icon, title, place, address }: ScheduleItemData) {
     <Reveal shrink={false}>
       <a href={href} className="">
         <div className="flex gap-4">
-          <div className="mt-1 flex h-13 w-11 items-center justify-center text-(--heart-color) rounded-2xl  bg-white">
+          <div className="mt-1 flex h-14 w-11 items-center justify-center text-(--heart-color) rounded-2xl  bg-white">
             {icon}
           </div>
           <div>
@@ -41,7 +39,7 @@ export default function Schedule({ items }: ScheduleProps) {
   const defaults: ScheduleItemData[] = [
     {
       href: "https://www.google.com/maps/place/Crkva+Uznesenja+Bla%C5%BEene+Djevice+Marije/@43.9387542,17.5823687,1121m/data=!3m2!1e3!4b1!4m6!3m5!1s0x475f0cb9d14f3971:0x3eb86acfc5cc03c8!8m2!3d43.9387542!4d17.5823687!16s%2Fm%2F0q3y0jq?entry=ttu&g_ep=EgoyMDI2MDEyNi4wIKXMDSoASAFQAw%3D%3D",
-      icon: <img src={prsten.src} alt="ring" className="h-13 w-10" />,
+      icon: <img src="/ring.png" alt="" className="h-13 w-11 " />,
       title: "18:00 VJENČANJE",
       place: "”UZNESENJE BLAŽENE DJEVICE MARIJE”",
       address: "Uskoplje",
